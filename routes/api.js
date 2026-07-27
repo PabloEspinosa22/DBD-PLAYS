886e1dccc62d7d4418dda2dbe6b18111578fdcd7
Pablo Espinosa Medel <gallo.sweiter@gmail.com>
2026-07-26 19:08:59 -0600

    Actualizacion de base de datos de killers y survivors con Shane

--- routes/api.js
+++ routes/api.js
@@ -3,13 +3,13 @@ const path = require('path');
 const app = express();
 const port = process.env.PORT || 3000;
 
-// Sirve los archivos de la carpeta public
-app.use(express.static(path.join(__dirname, 'public')));
+// El '../' le dice que salga de la carpeta 'routes' y busque 'public'
+app.use(express.static(path.join(__dirname, '../public')));
 
-// Rutas de las bases de datos
-app.get('/api/killers', (req, res) => res.json(require('./data/killers')));
-app.get('/api/survivors', (req, res) => res.json(require('./data/survivors')));
-app.get('/api/perks', (req, res) => res.json(require('./data/perks')));
+// Rutas de las bases de datos (también saliendo un nivel con '../')
+app.get('/api/killers', (req, res) => res.json(require('../data/killers')));
+app.get('/api/survivors', (req, res) => res.json(require('../data/survivors')));
+app.get('/api/perks', (req, res) => res.json(require('../data/perks')));
 
 // Iniciar el servidor
 app.listen(port, () => {
